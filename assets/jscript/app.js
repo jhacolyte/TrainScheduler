335f4a415db8abb46d82499f34b202bf33ea7f0f
Jhacolyte <toneth.tembrevilla@gmail.com>
2019-07-30 01:31:42 -0700

    working on current time and adding train on js script

--- assets/jscript/app.js
+++ assets/jscript/app.js
@@ -15,3 +15,55 @@ $(".form-field").on("keyup", function() {
     sessionStorage.setItem("time", timetemp);
     sessionStorage.setItem("freq", freqtemp);
 });
+
+$("#train-name").val(sessionStorage.getItem("train"));
+$("#destination").val(sessionStorage.getItem("city"));
+$("#first-train").val(sessionStorage.getItem("time"));
+$("#frequency").val(sessionStorage.getItem("freq"));
+
+$("#submit").on("click", function(event) {
+    event.preventDefault();
+
+    if ($("#train-name").val().trim() === "" ||
+        $("#destination").val().trim() === "" ||
+        $("#first-train").val().trim() === "" ||
+        $("#frequency").val().trim() === "") {
+
+        alert("Please fill in all details to add new train");
+
+    } else {
+
+        trainName = $("#train-name").val().trim();
+        destination = $("#destination").val().trim();
+        startTime = $("#first-train").val().trim();
+        frequency = $("#frequency").val().trim();
+
+        $(".form-field").val("");
+
+        database.ref().push({
+            trainName: trainName,
+            destination: destination,
+            frequency: frequency,
+            startTime: startTime,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+
+        sessionStorage.clear();
+    }
+
+});
+
+database.ref().on("child_added", function(childSnapshot) {
+            var startTimeConverted = moment(childSnapshot.val().startTime, "hh:mm").subtract(1, "years");
+            var timeDiff = moment().diff(moment(startTimeConverted), "minutes");
+            var timeRemain = timeDiff % childSnapshot.val().frequency;
+            var minToArrival = childSnapshot.val().frequency - timeRemain;
+            var nextTrain = moment().add(minToArrival, "minutes");
+            var key = childSnapshot.key;
+            var newrow = $("<tr>");
+            newrow.append($("<td>" + childSnapshot.val().trainName + "</td>"));
+            newrow.append($("<td>" + childSnapshot.val().destination + "</td>"));
+            newrow.append($("<td class='text-center'>" + childSnapshot.val().frequency + "</td>"));
+            newrow.append($("<td class='text-center'>" + moment(nextTrain).format("LT") + "</td>"));
+            newrow.append($("<td class='text-center'>" + minToArrival + "</td>"));
+            newrow.append($("<td class='text-center'><button class='arrival btn btn-danger btn-xs' data-key='" + key + "'>X</button></td>"));
