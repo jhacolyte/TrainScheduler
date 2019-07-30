0220a0a7ae48d662a705776d33c7a60b9712a2aa
Jhacolyte <toneth.tembrevilla@gmail.com>
2019-07-30 01:54:49 -0700

    still working with jscript figuring out how to use firebase

--- assets/jscript/app.js
+++ assets/jscript/app.js
@@ -54,16 +54,36 @@ $("#submit").on("click", function(event) {
 });
 
 database.ref().on("child_added", function(childSnapshot) {
-            var startTimeConverted = moment(childSnapshot.val().startTime, "hh:mm").subtract(1, "years");
-            var timeDiff = moment().diff(moment(startTimeConverted), "minutes");
-            var timeRemain = timeDiff % childSnapshot.val().frequency;
-            var minToArrival = childSnapshot.val().frequency - timeRemain;
-            var nextTrain = moment().add(minToArrival, "minutes");
-            var key = childSnapshot.key;
-            var newrow = $("<tr>");
-            newrow.append($("<td>" + childSnapshot.val().trainName + "</td>"));
-            newrow.append($("<td>" + childSnapshot.val().destination + "</td>"));
-            newrow.append($("<td class='text-center'>" + childSnapshot.val().frequency + "</td>"));
-            newrow.append($("<td class='text-center'>" + moment(nextTrain).format("LT") + "</td>"));
-            newrow.append($("<td class='text-center'>" + minToArrival + "</td>"));
-            newrow.append($("<td class='text-center'><button class='arrival btn btn-danger btn-xs' data-key='" + key + "'>X</button></td>"));
+    var startTimeConverted = moment(childSnapshot.val().startTime, "hh:mm").subtract(1, "years");
+    var timeDiff = moment().diff(moment(startTimeConverted), "minutes");
+    var timeRemain = timeDiff % childSnapshot.val().frequency;
+    var minToArrival = childSnapshot.val().frequency - timeRemain;
+    var nextTrain = moment().add(minToArrival, "minutes");
+    var key = childSnapshot.key;
+    var newrow = $("<tr>");
+    newrow.append($("<td>" + childSnapshot.val().trainName + "</td>"));
+    newrow.append($("<td>" + childSnapshot.val().destination + "</td>"));
+    newrow.append($("<td class='text-center'>" + childSnapshot.val().frequency + "</td>"));
+    newrow.append($("<td class='text-center'>" + moment(nextTrain).format("LT") + "</td>"));
+    newrow.append($("<td class='text-center'>" + minToArrival + "</td>"));
+    newrow.append($("<td class='text-center'><button class='arrival btn btn-danger btn-xs' data-key='" + key + "'>X</button></td>"));
+
+    if (minToArrival < 6) {
+        newrow.addClass("info");
+    }
+
+    $("#train-table-rows").append(newrow);
+
+});
+
+$(document).on("click", ".arrival", function() {
+    keyref = $(this).attr("data-key");
+    database.ref().child(keyref).remove();
+    window.location.reload();
+});
+
+currentTime();
+
+setInterval(function() {
+    window.location.reload();
+}, 60000);
